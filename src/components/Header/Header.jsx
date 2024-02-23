736d320dc58341039caf4d9ee1bd33f44841461a
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-02-22 21:48:57 -0500

    add hero

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import logo from '../../assets/logo/BrainFlix-logo.svg';
-import imgIcon from '../../assets/Images/Mohan-muruge.jpg';
+import avatar from '../../assets/Images/Mohan-muruge.jpg';
 import './Header.scss';
 const Header = () => {
     return (
@@ -11,8 +11,11 @@ const Header = () => {
                 <div className="header__search">
                     <input className="header__search__bar" type="search" placeholder="Search" />
                 </div>
+                <div className="header__btn2">
+                    <button className="header__btn2__upload2">UPLOAD</button>
+                </div>
                 <div className="header__image">
-                    <img className="header__image__man" src={imgIcon} alt="man" />
+                    <img className="header__image__avatar" src={avatar} alt="man" />
                 </div>
              </div>    
             <div className="header__btn">
