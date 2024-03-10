5ffd1511659312e0b41cca4e5437fd30e40be212
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-10 17:06:26 -0400

    add header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,17 +12,13 @@ const Header = () => {
             </Link>
             </div>
             <div className="header__wrapper">
-                <div className="header__search">
-                    <input className="header__search__bar" id="search" type="search" placeholder="Search" />
-                </div>
+                    <input className="header__bar" id="search" type="search" placeholder="Search" />
                 <div className="header__btn">
                 <Link className="header__link"to={`/upload`}>
                     <button className="header__btn__upload">UPLOAD</button>
                 </Link>
                 </div>
-                <div className="header__image">
-                    <img className="header__image__avatar" src={avatar} alt="man" />
-                </div>
+                    <img className="header__avatar" src={avatar} alt="man" />
              </div>    
         </header>
     );
